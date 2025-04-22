b113e303d3452551728750917bc03eb5d2f66626
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2025-04-22 19:40:21 -0300

    uhum

--- lexer.py
+++ lexer.py
@@ -1,18 +1,5 @@
 # Drayan Silva Magalhães, João Vitor Zambão, Lucas Gabriel Nunes Geremias, Lucca Lucchin de Campos Costa 
-import re
-
 class Lexer:
-    TOKEN_TYPES = [
-        ('OPERATORBINARIO', r'\\?(?:rightarrow|leftrightarrow|vee|wedge)'),
-        ('OPERATORUNARIO', r'\\neg'),
-        ('CONSTANTE', r'true|false'),
-        ('PROPOSICAO', r'[a-z]'),
-        ('ABREPAREN', r'\('),
-        ('FECHAPAREN', r'\)'),
-        ('ESPACO', r'\s+'),
-        ('ERRO', r'.')
-    ]
-
     def __init__(self):
         self.input = ''
         self.tokens = []
@@ -26,18 +13,55 @@ class Lexer:
     def tokenize(self, input_str):
         self.reset()
         self.input = input_str
-        while self.pos < len(self.input):
-            match = None
-            for token_type, regex in self.TOKEN_TYPES:
-                pattern = re.compile(regex)
-                match = pattern.match(self.input, self.pos)
-                if match:
-                    value = match.group(0)
-                    if token_type != 'ESPACO' and token_type != 'ERRO':
-                        self.tokens.append((token_type, value))
-                    self.pos = match.end()
-                    break
-            if not match:
-                raise ValueError(f"Caractere inválido: {self.input[self.pos]}")
-        
+        length = len(self.input)
+        while self.pos < length:
+            while self.pos < length and self.input[self.pos].isspace():
+                self.pos += 1
+            if self.pos >= length:
+                break
+
+            current_char = self.input[self.pos]
+
+            if current_char == '(':
+                self.tokens.append(('ABREPAREN', '('))
+                self.pos += 1
+            elif current_char == ')':
+                self.tokens.append(('FECHAPAREN', ')'))
+                self.pos += 1
+            elif current_char == '\\':
+                start = self.pos
+                self.pos += 1 
+                while self.pos < length and not self.input[self.pos].isspace() and self.input[self.pos] not in '()':
+                    self.pos += 1
+                operator = self.input[start:self.pos]
+                if operator == r'\neg':
+                    self.tokens.append(('OPERATORUNARIO', operator))
+                elif operator in (r'\rightarrow', r'\leftrightarrow', r'\vee', r'\wedge'):
+                    self.tokens.append(('OPERATORBINARIO', operator))
+                else:
+                    raise ValueError(f"Operador inválido: {operator}")
+            elif current_char.isalpha():
+                if current_char == 't':
+                    if (self.pos + 3 < length and
+                        self.input[self.pos+1:self.pos+4] == 'rue' and
+                        (self.pos +4 == length or self.input[self.pos+4].isspace() or self.input[self.pos+4] in '()')):
+                        self.tokens.append(('CONSTANTE', 'true'))
+                        self.pos += 4
+                    else:
+                        self.tokens.append(('PROPOSICAO', current_char))
+                        self.pos += 1
+                elif current_char == 'f':
+                    if (self.pos + 4 < length and
+                        self.input[self.pos+1:self.pos+5] == 'alse' and
+                        (self.pos +5 == length or self.input[self.pos+5].isspace() or self.input[self.pos+5] in '()')):
+                        self.tokens.append(('CONSTANTE', 'false'))
+                        self.pos += 5
+                    else:
+                        self.tokens.append(('PROPOSICAO', current_char))
+                        self.pos += 1
+                else:
+                    self.tokens.append(('PROPOSICAO', current_char))
+                    self.pos += 1
+            else:
+                raise ValueError(f"Caractere inválido: {current_char}")
         return self.tokens
--- main.py
+++ main.py
@@ -2,24 +2,38 @@
 from parser import ParserLL1
 from lexer import Lexer
 import re
+import sys
 
 def numbers_only(s):
     return bool(re.fullmatch(r"\d+", s))
 
-with open('data/single/example.txt') as f:
-    examples = [line.strip() for line in f.readlines()]
+if len(sys.argv) < 2:
+    print("Usage: python script.py <input_file>")
+    sys.exit(1)
 
-parser = ParserLL1()
-lexer = Lexer()
+input_file = sys.argv[1]
 
-for example in examples:
-    try:
-        if numbers_only(example): continue
+try:
+    with open(input_file) as f:
+        examples = [line.strip() for line in f.readlines()]
 
-        tokens = lexer.tokenize(example)
-        parser.parse(tokens)
+    lexer = Lexer()
+    parser = ParserLL1()
 
-        print(f"valida")
-    except SyntaxError:
-        print(f"invalida")
-        pass
+    for example in examples:
+        try:
+            if numbers_only(example): continue
+
+            tokens = lexer.tokenize(example)
+            parser.parse(tokens)
+
+            print(f"valida")
+        except SyntaxError:
+            print(f"invalida")
+            pass
+except FileNotFoundError:
+    print(f"Error: File '{input_file}' not found")
+    sys.exit(1)
+except IOError:
+    print(f"Error: Could not read file '{input_file}'")
+    sys.exit(1)
